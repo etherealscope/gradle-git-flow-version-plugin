acc148b11812994bd8945683183d94936ab09e5e
Jakub Venglar <venglar.jakub@gmail.com>
2020-10-05 10:35:35 +0200

    version 1.13.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.etherealscope"
-version = "1.12.0.RELEASE"
+version = "1.13.0.RELEASE"
 
 pluginBundle {
     website = "https://github.com/etherealscope/gradle-git-flow-version-plugin"
